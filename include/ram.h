c443b729fbf9ae6dac199839c08ce4e853334d94
Yanis42 <35189056+Yanis42@users.noreply.github.com>
2024-02-17 02:29:51 +0100

    Match every function from ``ram.s`` (#12)
    * ram split (ok)
    
    * matched ram.s
    
    * cleanup non_matchings
    
    * format
    
    * review
    
    * add back the EventFunc casts
    
    * ramGetBuffer: ``void*`` -> ``void**``
    
    * ramGetBuffer: yet another pointer change

---
+++ include/ram.h
@@ -0,0 +1,13 @@
+#ifndef _RAM_H
+#define _RAM_H
+
+#include "types.h"
+
+// __anon_0x4BFE7
+typedef struct Ram {
+    /* 0x00 */ void* pHost;
+    /* 0x04 */ void* pBuffer;
+    /* 0x08 */ u32 nSize;
+} Ram; // size = 0x0C
+
+#endif
