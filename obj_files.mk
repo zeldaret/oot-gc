7b0d47b14d13f4da945917d5b5c10804372df976
Yanis42 <35189056+Yanis42@users.noreply.github.com>
2024-02-17 15:14:05 +0100

    match codeGCN.s

--- obj_files.mk
+++ obj_files.mk
@@ -15,7 +15,7 @@ O_FILES :=                                  \
     $(BUILD_DIR)/asm/THPRead.o              \
     $(BUILD_DIR)/asm/THPVideoDecode.o       \
     $(BUILD_DIR)/asm/mcardGCN.o             \
-    $(BUILD_DIR)/asm/codeGCN.o              \
+    $(BUILD_DIR)/src/codeGCN.o              \
     $(BUILD_DIR)/asm/soundGCN.o             \
     $(BUILD_DIR)/asm/frame.o                \
     $(BUILD_DIR)/src/system.o               \
