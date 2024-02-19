872dbf075c2bd8bb9503d544424b7b4c72f84b8f
louist103 <louist103@gmail.com>
2024-02-18 23:00:04 -0500

    More fixes

--- src/sram.c
+++ src/sram.c
@@ -2,12 +2,9 @@
 #include "xlObject.h"
 #include "cpu.h"
 #include "system.h"
-#include "xlObject.h"
 
 int simulatorWriteSRAM(unsigned int address, unsigned char* data, int size);
 int simulatorReadSRAM(unsigned int address, unsigned char* data, int size);
-
-#define OBJ_NAME "SRAM"
 
 // __anon_0x74AB9
 typedef struct Sram {
@@ -16,12 +13,12 @@ typedef struct Sram {
 
 struct Ram;
 
-int sramEvent(void* pSram, s32 nEvent, void* pArgument);
+int sramEvent(Sram* pSram, s32 nEvent, void* pArgument);
 
 extern int ramGetBuffer(struct Ram* pRAM, void* ppRAM, u32 nOffset, u32* pnSize);
 extern int simulatorWriteSRAM(unsigned int address, unsigned char* data, int size);
 
-_XL_OBJECTTYPE gClassSram = {OBJ_NAME, sizeof(Sram), NULL, sramEvent};
+_XL_OBJECTTYPE gClassSram = {"SRAM", sizeof(Sram), NULL, (EventFunc)sramEvent};
 
 s32 sramCopySRAM(Sram* pSRAM, u32 nOffsetRAM, u32 nOffsetSRAM, u32 nSize) {
     void* pTarget;
@@ -47,47 +44,47 @@ s32 sramTransferSRAM(Sram* pSRAM, u32 nOffsetRAM, u32 nOffsetSRAM, u32 nSize) {
     return 1;
 }
 
-static int sramPut8(void* pObject, u32 nAddress, s8* pData) {
+static int sramPut8(Sram* pObject, u32 nAddress, s8* pData) {
     simulatorWriteSRAM(nAddress & 0x7FFF, (unsigned char*)pData, sizeof(s8));
     return 1;
 }
 
-static int sramPut16(void* pObject, u32 nAddress, s16* pData) {
+static int sramPut16(Sram* pObject, u32 nAddress, s16* pData) {
     simulatorWriteSRAM(nAddress & 0x7FFF, (unsigned char*)pData, sizeof(s16));
     return 1;
 }
 
-static int sramPut32(void* pObject, u32 nAddress, s32* pData) {
+static int sramPut32(Sram* pObject, u32 nAddress, s32* pData) {
     simulatorWriteSRAM(nAddress & 0x7FFF, (unsigned char*)pData, sizeof(s32));
     return 1;
 }
 
-static int sramPut64(void* pObject, u32 nAddress, s64* pData) {
+static int sramPut64(Sram* pObject, u32 nAddress, s64* pData) {
     simulatorWriteSRAM(nAddress & 0x7FFF, (unsigned char*)pData, sizeof(s64));
     return 1;
 }
 
-static int sramGet8(void* pObject, u32 nAddress, s8* pData) {
+static int sramGet8(Sram* pObject, u32 nAddress, s8* pData) {
     simulatorReadSRAM(nAddress & 0x7FFF, (unsigned char*)pData, sizeof(s8));
     return 1;
 }
 
-static int sramGet16(void* pObject, u32 nAddress, s16* pData) {
+static int sramGet16(Sram* pObject, u32 nAddress, s16* pData) {
     simulatorReadSRAM(nAddress & 0x7FFF, (unsigned char*)pData, sizeof(s16));
     return 1;
 }
 
-static int sramGet32(void* pObject, u32 nAddress, s32* pData) {
+static int sramGet32(Sram* pObject, u32 nAddress, s32* pData) {
     simulatorReadSRAM(nAddress & 0x7FFF, (unsigned char*)pData, sizeof(s32));
     return 1;
 }
 
-static int sramGet64(void* pObject, u32 nAddress, s64* pData) {
+static int sramGet64(Sram* pObject, u32 nAddress, s64* pData) {
     simulatorReadSRAM(nAddress & 0x7FFF, (unsigned char*)pData, sizeof(s64));
     return 1;
 }
 
-int sramEvent(void* pObject, s32 nEvent, void* pArgument) {
+int sramEvent(Sram* pObject, s32 nEvent, void* pArgument) {
     Sram* pSram = (Sram*)pObject;
 
     switch (nEvent) {
@@ -99,12 +96,12 @@ int sramEvent(void* pObject, s32 nEvent, void* pArgument) {
             break;
 
         case 0x1002:
-            if (!cpuSetDevicePut(((System*)pSram->pHost)->apObject[SOT_CPU], pArgument, sramPut8, sramPut16, sramPut32,
-                                 sramPut64)) {
+            if (!cpuSetDevicePut(((System*)pSram->pHost)->apObject[SOT_CPU], pArgument, (Put8Func)sramPut8,
+                                 (Put16Func)sramPut16, (Put32Func)sramPut32, (Put64Func)sramPut64)) {
                 return 0;
             }
-            if (!cpuSetDeviceGet(((System*)pSram->pHost)->apObject[SOT_CPU], pArgument, sramGet8, sramGet16, sramGet32,
-                                 sramGet64)) {
+            if (!cpuSetDeviceGet(((System*)pSram->pHost)->apObject[SOT_CPU], pArgument, (Get8Func)sramGet8,
+                                 (Get16Func)sramGet16, (Get32Func)sramGet32, (Get64Func)sramGet64)) {
                 return 0;
             }
         case 0x1003:
