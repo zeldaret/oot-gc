9f638800ca9da988dee5184dfb44bddf7134eacf
cadmic <cadmic24@gmail.com>
2024-02-20 23:37:33 -0800

    Create dolphin/thp.h

---
+++ include/dolphin/thp.h
@@ -0,0 +1,10 @@
+#ifndef _DOLPHIN_THP_H_
+#define _DOLPHIN_THP_H_
+
+#include "dolphin/types.h"
+
+s32 THPVideoDecode(void* file, void* tileY, void* tileU, void* tileV, void* work);
+BOOL THPInit(void);
+u32 THPAudioDecode(s16* buffer, u8* audioFrame, s32 flag);
+
+#endif
