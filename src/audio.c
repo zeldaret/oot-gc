26d075f44d8efc85e6e4acdb06339fe5809710a3
Yanis42 <35189056+Yanis42@users.noreply.github.com>
2024-02-21 22:42:45 +0100

    progress

--- src/audio.c
+++ src/audio.c
@@ -1,5 +1,7 @@
 #include "audio.h"
 #include "xlObject.h"
+#include "ram.h"
+#include "system.h"
 
 s32 audioEvent(Audio* pAudio, s32 nEvent, void* pArgument);
 
@@ -63,21 +65,73 @@ void* jtbl_800EE818[] = {
 
 char D_80135310[8] = "audio.c";
 
-#pragma GLOBAL_ASM("asm/non_matchings/audio/audioPut8.s")
+static s32 audioPut8(Audio* pAudio, u32 nAddress, s8* pData) { return 0; }
 
-#pragma GLOBAL_ASM("asm/non_matchings/audio/audioPut16.s")
+static s32 audioPut16(Audio* pAudio, u32 nAddress, s16* pData) { return 0; }
 
+#ifdef NON_MATCHING
 #pragma GLOBAL_ASM("asm/non_matchings/audio/audioPut32.s")
+#else
+s32 audioPut32(Audio* pAudio, u32 nAddress, s32* pData) {
+    // Parameters
+    // struct __anon_0x753E7* pAudio; // r31
+    // u32 nAddress; // r1+0xC
+    // s32* pData; // r1+0x10
 
-#pragma GLOBAL_ASM("asm/non_matchings/audio/audioPut64.s")
+    // Local variables
+    void* pBuffer; // r1+0x14
 
-#pragma GLOBAL_ASM("asm/non_matchings/audio/audioGet8.s")
+    switch (nAddress & 0x1F) {
+        case 0x0:
+            pAudio->nAddress = nAddress;
+            if (*pData & 0xFFFFFF != 0) {
+                if (!ramGetBuffer(((System*)pAudio->pHost)->apObject[SOT_RAM], &pBuffer, pAudio->nAddress, NULL)) {
+                    return 0;
+                }
+                if (pAudio->bEnable) {
+                    soundSetAddress(((System*)pAudio->pHost)->pSound, pBuffer);
+                }
+                break;
+            }
+            break;
+        case 0x4:
+            pAudio->nSize = *pData & 0x3FFF8;
+            if ((pAudio->nControl != 0) && pAudio->bEnable) {
+                soundSetLength(((System*)pAudio->pHost)->pSound, pAudio->nSize);
+            }
+            break;
+        case 0x8:
+            pAudio->nControl = *pData & 1;
+            break;
+        case 0xC:
+            xlObjectEvent(pAudio->pHost, 0x1001, (void*)7);
+            break;
+        case 0x10:
+            pAudio->nRateDAC = *pData & 0x3FFF;
+            if (pAudio->bEnable) {
+                soundSetDACRate(((System*)pAudio->pHost)->pSound, pAudio->nRateDAC);
+            }
+            break;
+        case 0x14:
+            pAudio->nRateBit = *pData & 0xF;
+            break;
+        default:
+            return 0;
+    }
 
-#pragma GLOBAL_ASM("asm/non_matchings/audio/audioGet16.s")
+    return 1;
+}
+#endif
+
+static s32 audioPut64(Audio* pAudio, u32 nAddress, s64* pData) { return 0; }
+
+static s32 audioGet8(Audio* pAudio, u32 nAddress, s8* pData) { return 0; }
+
+static s32 audioGet16(Audio* pAudio, u32 nAddress, s16* pData) { return 0; }
 
 #pragma GLOBAL_ASM("asm/non_matchings/audio/audioGet32.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/audio/audioGet64.s")
+static s32 audioGet64(Audio* pAudio, u32 nAddress, s64* pData) { return 0; }
 
 #pragma GLOBAL_ASM("asm/non_matchings/audio/audioEnable.s")
 
