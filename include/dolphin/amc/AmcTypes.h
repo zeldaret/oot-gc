0049662979540db6ec2998fdd990b2a83aa16217
cadmic <cadmic24@gmail.com>
2024-02-20 18:41:33 -0800

    Remove unused dolphin headers

--- include/dolphin/amc/AmcTypes.h
+++
@@ -1,9 +0,0 @@
-#ifndef _INCLUDE_DOLPHIN_AMCTYPES_H_
-#define _INCLUDE_DOLPHIN_AMCTYPES_H_
-
-#include "dolphin/os.h"
-
-// EXI callback function pointer type
-typedef void (*EXICallback)(s32 chan, OSContext* context);
-
-#endif
