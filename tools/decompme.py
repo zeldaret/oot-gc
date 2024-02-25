85ebb48b25c54ad356548accb60d201ab5671342
cadmic <cadmic24@gmail.com>
2024-02-25 12:40:24 -0800

    Handle more preprocessor directives in decompme script

--- tools/decompme.py
+++ tools/decompme.py
@@ -16,6 +16,7 @@ import re
 import urllib.parse
 import urllib.request
 
+INCLUDE_DIR = Path("include")
 COMPILER_NAME = "mwcc_233_159"
 # We don't set -inline deferred because otherwise the reversed function order
 # would require manually deleting all previous function definitions from the
@@ -23,35 +24,51 @@ COMPILER_NAME = "mwcc_233_159"
 COMPILER_FLAGS = (
     "-Cpp_exceptions off -proc gekko -fp hard -fp_contract on -enum int -O4,p -inline auto -nodefaults -msgstyle gcc"
 )
-INCLUDE_PATTERN = re.compile(r'^#include\s*[<"](.+?)[>"]$')
-GUARD_PATTERN = re.compile(r'^#ifndef\s+(.*)$')
-INCLUDE_DIR = Path("include")
 
-defines = set()
+INCLUDE_PATTERN = re.compile(r'^#include\s*[<"](.+?)[>"]$')
+DEFINE_PATTERN = re.compile(r'^#define\s+(\w+)(?:\s+(.*))?$')
+IF_PATTERN = re.compile(r'^#if(n)?(?:def)?\s+(.*)$')
+ELSE_PATTERN = re.compile(r'^#else$')
+ENDIF_PATTERN = re.compile(r'^#endif$')
+INCBIN_PATTERN = re.compile(r'^#pragma INCBIN\(.*\)$')
+
+# Defined preprocessor macros
+defines = {"__MWERKS__"}
+# Stack of preprocessor conditions
+condition_stack = [True]
 
 
 def process_file(path: Path) -> str:
     lines = path.read_text().splitlines()
     out_text = ""
     for i, line in enumerate(lines):
-        guard_match = GUARD_PATTERN.match(line.strip())
-        if i == 0 and guard_match:
-            if guard_match[1] in defines:
-                break
-            defines.add(guard_match[1])
-            continue
-
-        if line.startswith("#if") or line.startswith("#endif"):
-            continue
-
-        include_match = INCLUDE_PATTERN.match(line.strip())
-        if include_match:
-            out_text += f'/* "{path}" line {i + 1} "{include_match[1]}" */\n'
-            out_text += process_file(INCLUDE_DIR / include_match[1])
-            out_text += f'/* end "{include_match[1]}" */\n'
+        if (match := IF_PATTERN.match(line.strip())):
+            condition = match[2] in defines
+            if match[1] == "n":
+                condition = not condition
+            condition_stack.append(condition)
+        elif ELSE_PATTERN.match(line.strip()):
+            condition_stack[-1] = not condition_stack[-1]
+        elif ENDIF_PATTERN.match(line.strip()):
+            condition_stack.pop()
         else:
-            out_text += line
-            out_text += "\n"
+            if not all(condition_stack):
+                continue
+
+            if (match := DEFINE_PATTERN.match(line.strip())):
+                defines.add(match[1])
+                out_text += line
+                out_text += "\n"
+            elif (match := INCLUDE_PATTERN.match(line.strip())):
+                out_text += f'/* "{path}" line {i + 1} "{match[1]}" */\n'
+                out_text += process_file(INCLUDE_DIR / match[1])
+                out_text += f'/* end "{match[1]}" */\n'
+            elif INCBIN_PATTERN.match(line.strip()):
+                out_text += "    0"
+                out_text += "\n"
+            else:
+                out_text += line
+                out_text += "\n"
 
     return out_text
 
@@ -60,6 +77,7 @@ def main():
     parser = argparse.ArgumentParser(description="Create a decomp.me scratch")
     parser.add_argument("c_file", metavar="C_FILE", type=Path, help="Input .c file")
     parser.add_argument("asm_file", metavar="ASM_FILE", type=Path, help="Input .s file")
+    parser.add_argument("--print-context", action="store_true", help="Print the context and exit")
 
     args = parser.parse_args()
 
@@ -67,6 +85,10 @@ def main():
     asm_cont = args.asm_file.read_text()
     context = process_file(args.c_file)
     source_code = ""  # TODO: separate source code from context automatically
+
+    if args.print_context:
+        print(context)
+        return
 
     print("Uploading...")
     try:
