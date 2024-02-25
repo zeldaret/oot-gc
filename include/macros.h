fe2cc8522d2b06db5417a4a1653cea75afe2d998
cadmic <cadmic24@gmail.com>
2024-02-25 10:47:41 -0800

    Match cpuCompile functions (#39)
    * Match cpuCompile functions
    
    * Annotate with objdump disassembly

--- include/macros.h
+++ include/macros.h
@@ -6,6 +6,8 @@
 #define ARRAY_COUNT(arr) (s32)(sizeof(arr) / sizeof(arr[0]))
 #define ARRAY_COUNTU(arr) (u32)(sizeof(arr) / sizeof(arr[0]))
 
+#define OFFSETOF(p, field) ((u8*)&(p)->field - (u8*)(p))
+
 // Adds no-ops to increase a function's size, preventing automatic inlining
 #define NO_INLINE() \
     (void)0;        \
