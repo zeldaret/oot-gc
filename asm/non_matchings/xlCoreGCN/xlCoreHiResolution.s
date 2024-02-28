feabef47e54cedadfca47d09a4f2b947b5bbc554
Yanis42 <35189056+Yanis42@users.noreply.github.com>
2024-02-28 16:37:07 +0100

    few functions

--- asm/non_matchings/xlCoreGCN/xlCoreHiResolution.s
+++
@@ -1,3 +0,0 @@
-glabel xlCoreHiResolution
-/* 00033C 800058DC 38600001 */  li      r3, 1
-/* 000340 800058E0 4E800020 */  blr     
--- src/xlCoreGCN.c
+++ src/xlCoreGCN.c
@@ -5,9 +5,7 @@ u8 gTgPcTPL[] = {
 #pragma INCBIN("SIM_original.elf", 0x000D07A0, 0x00008081)
 };
 
-char D_800DB7A4[] = "xlCoreGCN.c";
 char D_800DB7B0[] = "CORE DONE!";
-char D_800DB7BC[] = "DEMOInit: invalid TV format\n";
 
 static GXRenderModeObj rmodeobj;
 GXTexObj g_texMap[4];
@@ -25,7 +23,12 @@ const f32 D_80135D04 = 0.0;
 const f32 D_80135D08 = 1.0;
 const f64 D_80135D10 = 4503599627370496.0;
 
-#pragma GLOBAL_ASM("asm/non_matchings/xlCoreGCN/xlCoreReset.s")
+s32 xlCoreReset(void) {
+    OSFreeToHeap(__OSCurrHeap, gpHeap);
+    OSSetArenaLo(gArenaLo);
+    OSSetArenaHi(gArenaHi);
+    return 1;
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/xlCoreGCN/xlCoreInitRenderMode.s")
 
@@ -33,11 +36,22 @@ const f64 D_80135D10 = 4503599627370496.0;
 
 #pragma GLOBAL_ASM("asm/non_matchings/xlCoreGCN/xlCoreInitGX.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/xlCoreGCN/xlCoreGetArgumentCount.s")
+s32 xlCoreGetArgumentCount(void) {
+    return gnCountArgument;
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/xlCoreGCN/xlCoreGetArgument.s")
+s32 xlCoreGetArgument(s32 iArgument, char** pszArgument) {
+    if ((iArgument >= 0) && (iArgument < gnCountArgument)) {
+        *pszArgument = *(gaszArgument + iArgument);
+        return 1;
+    }
 
-#pragma GLOBAL_ASM("asm/non_matchings/xlCoreGCN/xlCoreHiResolution.s")
+    return 0;
+}
+
+s32 xlCoreHiResolution(void) {
+    return 1;
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/xlCoreGCN/main.s")
 
