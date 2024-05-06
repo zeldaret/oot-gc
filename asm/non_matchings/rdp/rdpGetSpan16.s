d41bfbb5798af3a9900263293a5762f5f24884a8
Yanis <35189056+Yanis42@users.noreply.github.com>
2024-05-06 23:23:58 +0200

    Match remaining functions of ``rdp.c`` (#98)
    * fast matches
    
    * match rdp.c
    
    * format
    
    * remove useless files
    
    * review

--- asm/non_matchings/rdp/rdpGetSpan16.s
+++
@@ -1,3 +0,0 @@
-glabel rdpGetSpan16
-/* 06AB3C 800700DC 38600000 */  li      r3, 0
-/* 06AB40 800700E0 4E800020 */  blr     
--- asm/non_matchings/rdp/rdpPutSpan16.s
+++
@@ -1,3 +0,0 @@
-glabel rdpPutSpan16
-/* 06ABB8 80070158 38600000 */  li      r3, 0
-/* 06ABBC 8007015C 4E800020 */  blr     
