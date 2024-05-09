f721655b8d7125a57466a5b599b5c2a84d200585
Yanis <35189056+Yanis42@users.noreply.github.com>
2024-05-09 03:04:58 +0200

    Match most functions of ``pif.c`` (#101)
    * match most functions
    
    * match pifWriteRumble
    
    * match pifReadRumble
    
    * remove useless files
    
    * review

--- include/emulator/pif.h
+++ include/emulator/pif.h
@@ -4,6 +4,9 @@
 #include "dolphin.h"
 #include "emulator/xlObject.h"
 
+#define PIF_RAM_START 0x7C0
+#define PIF_RAM_END 0x7FF
+
 // __anon_0x3C277
 typedef enum ControllerType {
     CT_NONE = 0,
@@ -19,8 +22,8 @@ typedef enum ControllerType {
 
 // __anon_0x3C350
 typedef struct Pif {
-    /* 0x00 */ struct Rom* pROM;
-    /* 0x04 */ struct Ram* pRAM;
+    /* 0x00 */ void* pROM;
+    /* 0x04 */ void* pRAM;
     /* 0x08 */ void* pHost;
     /* 0x0C */ u16 controllerType[5];
     /* 0x16 */ char controllerStatus[5];
@@ -33,6 +36,9 @@ bool pifSetControllerType(Pif* pPIF, s32 channel, ControllerType type);
 bool pifGetEControllerType(Pif* pPIF, s32 channel, ControllerType* type);
 bool pifSetEEPROMType(Pif* pPIF, ControllerType type);
 bool pifGetEEPROMSize(Pif* pPIF, u32* size);
+bool pifExecuteCommand(Pif* pPIF, u8* buffer, s32 unused, u8* prx, s32 channel);
+bool pifProcessInputData(Pif* pPIF);
+bool pifProcessOutputData(Pif* pPIF);
 bool pifSetData(Pif* pPIF, u8* acData);
 bool pifGetData(Pif* pPIF, u8* acData);
 bool pifEvent(Pif* pPIF, s32 nEvent, void* pArgument);
