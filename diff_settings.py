6cef9e2828be1529260f91b905714bd529a868c9
cadmic <cadmic24@gmail.com>
2024-05-18 15:54:04 -0700

    Make diff.py multi-version but remove target line-number support

--- diff_settings.py
+++ diff_settings.py
@@ -1,9 +1,12 @@
+def add_custom_arguments(parser):
+    parser.add_argument("-v", "--version", help="Emulator version to diff", default="ce-j")
+
 def apply(config, args):
-    config["mapfile"] = f"build/ce-j/oot-gc.elf.MAP"
+    version = args.version
+    config["mapfile"] = f"build/{version}/oot-gc.elf.MAP"
     config["source_directories"] = ["src", "include"]
     config["arch"] = "ppc"
     config["map_format"] = "mw" # gnu, mw, ms
-    config["build_dir"] = f"build/ce-j/src" # only needed for mw and ms map formats
-    config["objdump_executable"] = "tools/objdump_wrapper.py"
+    config["build_dir"] = f"build/{version}/src" # only needed for mw and ms map formats
+    config["objdump_executable"] = "build/binutils/powerpc-eabi-objdump"
     config["show_line_numbers_default"] = True
-    config["show_target_line_numbers_default"] = True
