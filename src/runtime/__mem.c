1d03b84d3d053b6bc551f16ea23f97c58a289b0a
Yanis <35189056+Yanis42@users.noreply.github.com>
2024-05-26 17:11:52 +0200

    Match __mem.c and runtime.c (#153)
    * match more of runtime
    
    * mark matched for mq
    
    * format
    
    * revert stdarg changes
    
    * review

---
+++ src/runtime/__mem.c
@@ -0,0 +1,84 @@
+#include "dolphin/types.h"
+#include "mem_funcs.h"
+
+INIT void* memset(void* dst, int val, size_t n) {
+    __fill_mem(dst, val, n);
+
+    return (dst);
+}
+
+INIT void __fill_mem(void* dst, int val, size_t n) {
+    u32 v = (u8)val;
+    u32 i;
+
+    dst = (void*)((u8*)dst - 1);
+
+    if (n >= 32) {
+        i = (~(u32)dst) & 3;
+
+        if (i) {
+            n -= i;
+
+            do {
+                dst = (void*)((u8*)dst + 1);
+                *(u8*)dst = v;
+            } while (--i);
+        }
+
+        if (v) {
+            v |= v << 24 | v << 16 | v << 8;
+        }
+
+        dst = (void*)((u32*)((u8*)dst + 1) - 1);
+        i = n >> 5;
+
+        if (i) {
+            do {
+                *++(((u32*)dst)) = v;
+                *++(((u32*)dst)) = v;
+                *++(((u32*)dst)) = v;
+                *++(((u32*)dst)) = v;
+                *++(((u32*)dst)) = v;
+                *++(((u32*)dst)) = v;
+                *++(((u32*)dst)) = v;
+                *++(((u32*)dst)) = v;
+            } while (--i);
+        }
+
+        i = (n & 31) >> 2;
+
+        if (i) {
+            do {
+                dst = (void*)((u32*)dst + 1);
+                *(u32*)dst = v;
+            } while (--i);
+        }
+
+        dst = (void*)((u8*)((u32*)dst + 1) - 1);
+        n &= 3;
+    }
+
+    if (n) {
+        do {
+            dst = (void*)((u8*)dst + 1);
+            *(u8*)dst = v;
+        } while (--n);
+    }
+}
+
+INIT void* memcpy(void* dst, const void* src, size_t n) {
+    const char* p;
+    char* q;
+    int rev = ((u32)src < (u32)dst);
+
+    if (!rev) {
+
+        for (p = (const char*)src - 1, q = (char*)dst - 1, n++; --n;)
+            *++q = *++p;
+
+    } else {
+        for (p = (const char*)src + n, q = (char*)dst + n, n++; --n;)
+            *--q = *--p;
+    }
+    return (dst);
+}
