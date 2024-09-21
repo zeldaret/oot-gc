29f1dd1347f2fecee074c2a2f055e1f4f77355c2
Yanis42 <35189056+Yanis42@users.noreply.github.com>
2024-09-21 14:23:57 +0200

    fix OS issues

--- include/dolphin/os/OSRtc.h
+++ include/dolphin/os/OSRtc.h
@@ -10,6 +10,7 @@ bool __OSUnlockSramEx(bool commit);
 bool __OSSyncSram(void);
 u32 OSGetSoundMode(void);
 void OSSetSoundMode(u32 mode);
+u8 OSGetLanguage(void);
 u16 OSGetWirelessID(s32 channel);
 void OSSetWirelessID(s32 channel, u16 id);
 u16 OSGetGbsMode(void);
