837dfdf47717fe359cd10f125d80cfaab036d0a0
Yanis42 <35189056+Yanis42@users.noreply.github.com>
2024-09-21 22:12:36 +0200

    review

--- libc/string.h
+++ libc/string.h
@@ -5,12 +5,11 @@
 
 char* strcpy(char* dst, const char* src);
 char* strncpy(char* dst, const char* src, size_t num);
-char* strncat(char* dest, const char* src, size_t n);
+char* strcat(char* dest, const char* src);
 size_t strlen(const char* s);
 int strcmp(const char* s1, const char* s2);
 int strncmp(const char* s1, const char* s2, size_t n);
 char* strchr(const char* str, int chr);
-char* strcat(char* dest, const char* src);
 
 void* memchr(const void* p, int val, size_t n);
 int memcmp(const void* p1, const void* p2, size_t n);
