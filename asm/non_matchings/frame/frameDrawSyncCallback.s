a561eda9846d8511111d382b07cbea86197dce5d
cadmic <cadmic24@gmail.com>
2025-04-11 20:48:25 -0700

    Delete asm/non_matchings/frame/

--- asm/non_matchings/frame/frameDrawSyncCallback.s
+++
@@ -1,8 +0,0 @@
-glabel frameDrawSyncCallback
-/* 026DC0 8002C360 5460043E */  clrlwi  r0, r3, 0x10
-/* 026DC4 8002C364 28007D00 */  cmplwi  r0, 0x7d00
-/* 026DC8 8002C368 4082000C */  bne     lbl_8002C374
-/* 026DCC 8002C36C 38000001 */  li      r0, 1
-/* 026DD0 8002C370 900D89C4 */  stw     r0, sCopyFrameSyncReceived@sda21(r13)
-lbl_8002C374:
-/* 026DD4 8002C374 4E800020 */  blr     
--- asm/non_matchings/frame/frameSetDepth.s
+++
@@ -1,5 +0,0 @@
-glabel frameSetDepth
-/* 020A44 80025FE4 D0230124 */  stfs    f1, 0x124(r3)
-/* 020A48 80025FE8 D0430128 */  stfs    f2, 0x128(r3)
-/* 020A4C 80025FEC 38600001 */  li      r3, 1
-/* 020A50 80025FF0 4E800020 */  blr     
