f514d4499768295a8e02dddb1dfeebb2cdb43bc8
Yanis002 <35189056+Yanis002@users.noreply.github.com>
2025-05-16 13:07:16 +0200

    remove unused headers and fix build issues

--- include/dolphin/demo.h
+++ include/dolphin/demo.h
@@ -1,12 +1,10 @@
 #ifndef _DOLPHIN_DEMO_H_
 #define _DOLPHIN_DEMO_H_
 
-#include "dolphin/demo/DEMOAVX.h"
 #include "dolphin/demo/DEMOInit.h"
 #include "dolphin/demo/DEMOPad.h"
 #include "dolphin/demo/DEMOPuts.h"
 #include "dolphin/demo/DEMOStats.h"
-#include "dolphin/demo/DEMOWin.h"
 
 #ifdef __cplusplus
 extern "C" {
